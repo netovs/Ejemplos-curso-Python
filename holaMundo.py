8aecc86636fd4cb4dba68999b96452ee07fd37d2
Néstor Velázquez <netovs@gmail.com>
2011-07-13 04:51:51 -0500

    Ejemplo del curso de python con linea de edad.

--- holaMundo.py
+++ holaMundo.py
@@ -1,3 +1,9 @@
 #!/usr/bin/python
 nombre=raw_input("Cual es tu nombre: ");
 print "Hola " + nombre + " bienvenido al mundo python";
+edad = int(raw_input('Cual es tu edad: '));
+if edad < 18: 
+      print 'No tienes edad suficiente';
+else: 
+      print 'Es legal que veas porno.';
+
